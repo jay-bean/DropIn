144c39d72933cdcd8f45291a60345600c4734531
Jay Hutts <janieehutts@gmail.com>
2022-08-03 08:49:28 -0600

    added hearts

--- frontend/src/components/Skateparks/Skatepark.js
+++ frontend/src/components/Skateparks/Skatepark.js
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
+import './skatepark-card.css';
 
 function Skatepark({ skatepark }) {
   return (
     skatepark &&
-      <div>
-        {skatepark.images.length > 0 && (<Link to={`/skateparks/${skatepark.id}`}><img src={skatepark.images[0].url} style={{width: '300px'}}/></Link>)}
-        <div>{skatepark.name}</div>
-        <div>{skatepark.description}</div>
+      <div className='explore-page-cards'>
+        {skatepark.images.length > 0 && (<Link to={`/skateparks/${skatepark.id}`}><img src={skatepark.images[0].url} style={{width: '200px'}}/></Link>)}
+        <Link to={`/skateparks/${skatepark.id}`}><div>{skatepark.name}</div></Link>
         <div>{skatepark.address}</div>
         <div>{skatepark.city}, {skatepark.state} {skatepark.zipcode}</div>
+        <img src="https://img.icons8.com/material/48/000000/hearts--v1.png"/>
+        <img src="https://img.icons8.com/material-outlined/48/000000/hearts.png"/>
       </div>
   );
 }
